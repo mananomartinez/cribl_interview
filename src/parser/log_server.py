484a83bd169d7354857b0ce033f7c2ca53199a1c
Jose Luis Martinez <jose.martinez@mananofoto.com>
2025-01-12 16:19:27 -0800

    Updated endpoints to validate query parameters are passed and coresponding errors if not passed. Additionally, added code to remove any quotes that might have been added to the query.

--- src/parser/log_server.py
+++ src/parser/log_server.py
@@ -19,6 +19,11 @@ def get_all_logs():
 @app.route('/log')
 def get_single_log_file():
     file_name = request.args.get('file')
+    if not file_name:
+        return make_response("No file name provided.", 400)
+    
+    # Remove quotes from the file_name, if any. 
+    file_name = file_name.replace('"', '').replace("'", "")
     
     # Construct the dir since the file name is not a full path
     dir = os.environ.get('LOG_DIRECTORY', '/var/log')
@@ -32,6 +37,11 @@ def get_single_log_file():
 @app.route('/search')
 def search_logs():
     keyword = request.args.get('keyword')
+    if not keyword:
+        return make_response("No keyword provided.", 400)
+    
+    # Remove quotes from the keyword, if any. 
+    keyword = keyword.replace('"', '').replace("'", "")
     results = search_directory(keyword)
     
     if type(results) == dict:
