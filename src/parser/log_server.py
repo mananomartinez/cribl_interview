3775e09e106ecd1c5807d449f17675294a7109f2
mananiux <jlm2454@rit.edu>
2025-01-14 13:55:18 -0800

    Removed references to the /logs endpoint and added the /log/<file>?entries= method for returning a specific number of entries in a log file.

--- src/parser/log_server.py
+++ src/parser/log_server.py
@@ -1,20 +1,12 @@
 import os
 from flask import Flask, request, make_response
-from parser import read_all_log_files, read_log_file, search_directory
+from parser import read_single_file, search_directory, read_n_log_entries
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "This is a log-reading server as part of the interview process for Cribl."
-
-@app.route('/logs')
-def get_all_logs():
-    results = read_all_log_files()
-    if results:
-        return results
-    else:
-        return make_response("No readable log files found.", 404)
 
 @app.route('/log')
 def get_single_log_file():
@@ -24,11 +16,17 @@ def get_single_log_file():
     
     # Remove quotes from the file_name, if any. 
     file_name = file_name.replace('"', '').replace("'", "")
+    log_directory = os.environ.get('LOG_DIRECTORY', '/var/log')
     
-    # Construct the dir since the file name is not a full path
-    dir = os.environ.get('LOG_DIRECTORY', '/var/log')
+    # Check if the log_directory is already defined in the file_name
+    if (file_name.find(log_directory) == -1):    
+        file_path = log_directory + "/" + file_name
+    else: 
+        file_path = file_name
+
+    
     results = {}
-    read_log_file(dir + "/" + file_name, results)
+    read_single_file(file_path, results)
     
     if results:
         return results
@@ -49,3 +47,20 @@ def search_logs():
         return results 
     else:
         return make_response(results, 404)
+    
+
+@app.route('/log/<file>')
+def read_number_of_entries(file: str):
+    entries = int(request.args.get('entries'))
+    
+    if not entries:
+        return make_response("No number of enties provided.", 400)
+    
+    # Remove quotes from the keyword, if any. 
+    results = read_n_log_entries(file, entries)
+    
+    if type(results) == dict:
+        return results 
+    else:
+        return make_response(results, 404)
+
