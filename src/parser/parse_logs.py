a03054a57f7fa6fa661643d74d17548c06fc274e
Jose Luis Martinez <jose.martinez@mananofoto.com>
2025-01-14 16:24:58 -0800

    Added back the code to parse all possible log files in the /var/log directory

--- src/parser/parse_logs.py
+++ src/parser/parse_logs.py
@@ -1,8 +1,30 @@
 import os
 from concurrent.futures import ThreadPoolExecutor
-from collections import deque
 from typing import Generator
 
+
+def read_all_log_files(n_entries: int = 0) -> list:
+    """
+    Traverses a directory and parses every log file within it. 
+    Returns a list of dictionaries, each of which uses the file path
+    as the key and the logs as list of entries.
+    """
+    log_directory = os.environ.get('LOG_DIRECTORY', '/var/log')
+
+    all_log_entries = {}
+    tasks = []
+    # Traverse directory and read each log file in a separate thread
+    with ThreadPoolExecutor() as executor:
+        for root, _, files in os.walk(log_directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                task = executor.submit(read_single_file, file_path, all_log_entries)
+                tasks.append(task)
+        # Wait for each file search task to complete
+        for task in tasks:
+            task.result()
+    # Display aggregated logs
+    return all_log_entries
 
 def read_single_file(file_path: str, all_log_entries: dict) -> None:
     """
